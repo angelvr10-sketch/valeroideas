08512419b5bb12f8ca358731f4543df275a3e866
angelvr10-sketch <angelvr10@gmail.com>
2025-12-21 18:14:05 -0600

    Fix: robust database connection

--- core/settings.py
+++ core/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # <-- Agrega esta línea
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -82,11 +85,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
+# Verificación de seguridad: si DATABASE_URL no tiene 'postgres', usamos sqlite (local)
+if not DATABASE_URL:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 
 
@@ -136,3 +149,8 @@ CSRF_TRUSTED_ORIGINS.append('http://192.168.100.6:8000')
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False  # Esto ayuda a que el navegador la acepte más fácil
 SESSION_COOKIE_SECURE = False
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
